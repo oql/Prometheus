e0f39c5f2aff2483bcfe231b12e0e0edda239573
GgoGgo <cfr2ak@gmail.com>
2015-04-11 16:49:26 +0900

    fix query error, redirect user to right page(trying)

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ container.deployVerticle("init_database.js");
 
 var http = vertx.createHttpServer().requestHandler(function(req){
     if(req.uri() === "/"){
-        console.log('--index--');
         req.response.sendFile("index.html");
     } else if(req.uri() === "/signin"){
-        console.log("--signin--");
         req.response.sendFile("edit.html");
 
         req.expectMultiPart(true);
@@ -23,17 +21,24 @@ var http = vertx.createHttpServer().requestHandler(function(req){
                 'mysql.test',
                 {
                     action: 'select',
-                    stmt: "select * from user where email="+"'em'"
+                    stmt: "select * from user where email='"+em+"'"
                 },
                 function(msg){
-                    console.log('--sql result--');
-                    console.log('status: '+msg.status);
-                    console.log(msg.result[0].pid);
+                    console.log('---sql status: '+msg.status+'---');
 
-                    // if(pw == msg.result.password){
-                    //     console.log("###correct###");
-                    //     // <script></script>
-                    // }
+                    if(pw != msg.result[0].password){
+                        console.log("--incorrect!!--");
+                        //back to index page
+                        eb.send('go.index', 'message', function(rpl){
+
+                        });
+                    } else{
+                        console.log("###correct###");
+                        //get user to main page
+                        eb.send('go.main', 'message', function(rpl){
+                            
+                        });
+                    }
                 }
             );
         });
@@ -43,23 +48,29 @@ var http = vertx.createHttpServer().requestHandler(function(req){
         req.expectMultiPart(true);
         req.endHandler(function(){
             var attrs = req.formAttributes();
+            var em = attrs.get("em");
+            var pw = attrs.get("pw");
+            var nk = attrs.get("nk");
+
             eb.send(
                 'mysql.test',
                 {
                     action: 'insert',
-                    stmt: "insert into t1(pid, name) values("+"3"+","+"'ketty'"+")"
+                    stmt: "insert into user(email, password, nickname) values('"+em+"','"+pw+"','"+nk+"')"
                 },
                 function(msg){
-                    console.log('--sql result--');
-                    console.log('status: '+msg.status);
-
-                    // SELECT
-                    for(var i in msg.result){
-                        console.log("pid: "+msg.result[i].pid + ", name: "+msg.result[i].name);
-                    }
-                    // console.log("pid: "+msg.result[0].pid+", name: "+msg.result[0].name);
+                    console.log('---sql status: '+msg.status+'---');
+                    //get user to main page
                 }
             );
         });
+    } else if(req.uri() === "/main"){
+        req.response.sendFile("main.html");
     }
+
+    eb.registerHandler('go.main', function(msg, replier){
+        console.log("--got go.main event--");
+        // req.response.write("<script>location.href='/main';</script>");
+        replier(req.response.write("<script>location.href='/main';</script>"));
+    });
 }).listen(8080);
