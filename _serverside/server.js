f8f3cbf455c25e47ee1173b5df09a261202bf8fb
Ggoggo <Ggoggo@gmail.com>
2015-04-13 08:46:35 +0900

    simple comment added

--- _serverside/server.js
+++ _serverside/server.js
@@ -5,13 +5,18 @@ var eb = require("vertx/event_bus");
 
 container.deployVerticle("init_database.js");
 
+//load query functions( ex q_signin, q_signup )
 load("queryset.js");
 
+//create Http server that listen port number 8080
 var http = vertx.createHttpServer().requestHandler(function(req){
+    //route user by their uri
     if(req.uri() === "/"){
         req.response.sendFile("index.html");
     } else if(req.uri() === "/signin"){
         req.response.sendFile("edit.html");
+
+        //get Form data by 'POST' method
         req.expectMultiPart(true);
         req.endHandler(function(){
             var attrs = req.formAttributes();
@@ -22,6 +27,8 @@ var http = vertx.createHttpServer().requestHandler(function(req){
         });
     } else if(req.uri() === "/signup"){
         req.response.sendFile("edit.html");
+
+        //get Form data by 'POST' method
         req.expectMultiPart(true);
         req.endHandler(function(){
             var attrs = req.formAttributes();
@@ -29,7 +36,7 @@ var http = vertx.createHttpServer().requestHandler(function(req){
             var pw = attrs.get("pw");
             var nk = attrs.get("nk");
 
-            query("insert into user(email, password, nickname) values('"+em+"','"+pw+"','"+nk+"')");
+            q_signup("insert into user(email, password, nickname) values('"+em+"','"+pw+"','"+nk+"')");
         });
     } else if(req.uri() === "/main"){
         req.response.sendFile("main.html");
