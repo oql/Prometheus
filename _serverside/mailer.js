0fc89564e00b2c680e95f144c1360fbfe8154398
Ggoggo <cfr2ak@gmail.com>
2015-05-08 20:05:37 +0900

    classify mailauth

--- _serverside/mailer.js
+++ _serverside/mailer.js
@@ -98,66 +98,6 @@ mailer.prototype.sendAuthMail = function(req){
             );
         }
     ]);
-
-    // eb.send(
-    //     'redis.io',
-    //     {
-    //         command: "get",
-    //         args: [uuid]
-    //     },
-    //     function(msg){
-    //         if(msg.value==null){
-    //             console.log("failed to get session(send_auth_mail()): ");
-    //         }else{
-    //             nk = msg.value;
-    //             // remove user from database
-    //             eb.send(
-    //                 'maria.io',
-    //                 {
-    //                     action: 'select',
-    //                     stmt: "select * from user where nickname=?",
-    //                     values: [[nk]]
-    //                 },
-    //                 function(msg){
-    //                     if(msg.status == 'ok'){
-    //                         em = msg.result[0].email;
-    //                         code = owner.generateCode();
-    //                         eb.send(
-    //                             'redis.io',
-    //                             {
-    //                                 command: "set",
-    //                                 args: [em, code]
-    //                             },
-    //                             function(msg){
-    //                                 if(msg.status == 'ok'){
-    //                                     eb.send(
-    //                                         'redis.io',
-    //                                         {
-    //                                             command: "expire",
-    //                                             args: [em, server["sesn_lifetime"]]
-    //                                         },
-    //                                         function(msg){
-    //                                             console.log("email-code set in redis(sendAuthMail()): "+msg.status);
-    //                                             eb.send(
-    //                                                 'mail.io',
-    //                                                 {
-    //                                                     "from": "auth@ryunwake.com",
-    //                                                     "to": em,
-    //                                                     "subject": "Auth mail",
-    //                                                     "body": code+""
-    //                                                 }
-    //                                             );
-    //                                         }
-    //                                     );
-    //                                 }
-    //                             }
-    //                         );
-    //                     }
-    //                 }
-    //             );
-    //         }
-    //     }
-    // );
 };
 
 mailer.prototype.checkMailCode = function(req){
@@ -169,62 +109,79 @@ mailer.prototype.checkMailCode = function(req){
         var nk = null;
         var uuid = sesn.getCookieUUID(req);
         console.log("code(checkMailCode()): "+code);
-        eb.send(
-            'redis.io',
-            {
-                command: "get",
-                args: [uuid]
-            },
-            function(msg){
-                nk = msg.value;
-                console.log("gotten nickname(checkMailCode()): "+ nk);
-                if( nk!= null){
-                    eb.send(
-                        'maria.io',
-                        {
-                            action: 'select',
-                            stmt: "select * from user where nickname=?",
-                            values: [[nk]]
-                        },
-                        function(msg){
-                            if(msg.status == 'ok'){
-                                em = msg.result[0].email;
-                                console.log("gotten email(checkMailCode()): "+em);
-                                eb.send(
-                                    'redis.io',
-                                    {
-                                        command: "get",
-                                        args: [em]
-                                    },
-                                    function(msg){
-                                        console.log("code value from redis(checkMailCode()): "+msg.value);
-                                        if(msg.value == code){
-                                            eb.send(
-                                                'maria.io',
-                                                {
-                                                    action: 'update',
-                                                    stmt: "update user set authed='true' where nickname=?",
-                                                    values: [[nk]]
-                                                },
-                                                function(msg){
-                                                    if(msg.status == 'ok'){
-                                                        req.response.end("<script>location.href = '"+server['url']+"/main';</script>");
-                                                    }
-                                                }
-                                            );
-                                        }else{
-                                            req.response.end("<script>location.href = '"+server['url']+"';</script>");
-                                        }
-                                    }
-                                );
-                            }
+
+        wtf.heap([
+            uuid
+        ],
+        [
+            function(uuid, cb){
+                eb.send(
+                    'redis.io',
+                    {
+                        command: "get",
+                        args: [uuid]
+                    },
+                    function(msg){
+                        nk = msg.value;
+                        console.log("gotten nickname(checkMailCode()): "+ nk);
+                        if( nk!= null){
+                            cb(nk);
+                        }else{
+                            req.response.end("<script>location.href = '"+server['url']+"';</script>");
                         }
-                    );
-                } else{
-                    req.response.end("<script>location.href = '"+server['url']+"';</script>");
-                }
+                    }
+                );
+            },
+            function(nk, cb){
+                eb.send(
+                    'maria.io',
+                    {
+                        action: 'select',
+                        stmt: "select * from user where nickname=?",
+                        values: [[nk]]
+                    },
+                    function(msg){
+                        if(msg.status == 'ok'){
+                            em = msg.result[0].email;
+                            console.log("gotten email(checkMailCode()): "+em);
+                            cb(em);
+                        }
+                    }
+                );
+            },
+            function(em, cb){
+                eb.send(
+                    'redis.io',
+                    {
+                        command: "get",
+                        args: [em]
+                    },
+                    function(msg){
+                        console.log("code value from redis(checkMailCode()): "+msg.value);
+                        if(msg.value == code){
+                            cb(nk);
+                        }else{
+                            req.response.end("<script>location.href = '"+server['url']+"';</script>");
+                        }
+                    }
+                );
+            },
+            function(nk, cb){
+                eb.send(
+                    'maria.io',
+                    {
+                        action: 'update',
+                        stmt: "update user set authed='true' where nickname=?",
+                        values: [[nk]]
+                    },
+                    function(msg){
+                        if(msg.status == 'ok'){
+                            req.response.end("<script>location.href = '"+server['url']+"/main';</script>");
+                        }
+                    }
+                );
             }
-        );
+        ]);
     });
 };
 
